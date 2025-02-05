68619a37adb0bf95b55ffa8a48bbb3201ceb7c3a
唐诗凯 <2434102007@qq.com>
2025-02-05 16:41:42 +0800

    init

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -80,7 +80,7 @@ class PureHttp {
               const data = getToken();
               if (data) {
                 const now = new Date().getTime();
-                const expired = parseInt(data.expires) - now <= 0;
+                const expired = data.expires - now <= 0;
                 if (expired) {
                   if (!PureHttp.isRefreshing) {
                     PureHttp.isRefreshing = true;
